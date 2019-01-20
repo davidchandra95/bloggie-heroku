742a952f5268ae47994fcec196bfded31a4896ac
davidchandra95 <davidchandra95@yahoo.com>
2019-01-20 17:00:08 +0700

    fix sign up

--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,43 @@ const User = require('../models/user');
 const passport = require('passport');
 
 router.get('/', (req, res) => {
-   res.redirect('post');
+  res.redirect('post');
 });
 
 router.get('/register', (req, res) => {
-   res.render('register');
+  res.render('register');
 });
 
-router.post('/register/', (req, res) => {
-   let newUser = new User({
-      email: req.body.email,
-      username: req.body.username
-   });
-   User.register(newUser, req.body.password, (err, user) => {
-      if (err) return res.redirect('/register', {status: 'false', message: err});
+router.post('/register', (req, res) => {
+  let newUser = new User({
+    email: req.body.email,
+    username: req.body.username
+  });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) return res.redirect('/register', { status: 'false', message: err });
 
-      passport.authenticate('local')(req, res, () => {
-         res.redirect('/');
-      });
-   });
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/');
+    });
+  });
 });
 
 router.get('/login', (req, res) => {
-   res.render('login');
+  res.render('login');
 });
 
 router.post('/login', passport.authenticate('local', {
-   // successRedirect: req.session.redirectTo,
-   failureRedirect: '/login'
+  // successRedirect: req.session.redirectTo,
+  failureRedirect: '/login'
 }), (req, res) => {
-   res.redirect(req.session.redirectTo);
-   delete req.session.redirectTo;
+  res.redirect(req.session.redirectTo);
+  delete req.session.redirectTo;
 });
 
 router.get('/logout', (req, res) => {
-   req.logout();
-   req.flash('success', "You've been logged out");
-   res.redirect('login');
+  req.logout();
+  req.flash('success', "You've been logged out");
+  res.redirect('login');
 });
 
 module.exports = router;
